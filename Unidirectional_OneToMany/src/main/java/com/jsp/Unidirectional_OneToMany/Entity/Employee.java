23f53911a9f8eecc2645c5ce18648727a3d8a95d
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-02-19 23:58:50 +0530

    Add files via upload

---
+++ Unidirectional_OneToMany/src/main/java/com/jsp/Unidirectional_OneToMany/Entity/Employee.java
@@ -0,0 +1,75 @@
+package com.jsp.Unidirectional_OneToMany.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Employee {
+
+	@Id
+	private int id;
+	private String name;
+	private int age;
+	
+	@OneToOne
+    private Department department_id;
+	
+	
+	public Employee() {
+	}
+
+
+	public Employee(int id, String name, int age, Department department_id) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.department_id = department_id;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public Department getDepartment_id() {
+		return department_id;
+	}
+
+
+	public void setDepartment_id(Department department_id) {
+		this.department_id = department_id;
+	}
+	
+	
+	
+	
+	
+}
