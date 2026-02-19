20368d53aff4fef51ff9e5c88487abb41e5862e2
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-02-20 01:35:35 +0530

    Add files via upload

---
+++ Unidirectional_OneToOne/src/main/java/com/jsp/Unidirectional_OneToOne/Driver/Driver.java
@@ -0,0 +1,41 @@
+package com.jsp.Unidirectional_OneToOne.Driver;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.jsp.Unidirectional_OneToOne.Entity.Department;
+import com.jsp.Unidirectional_OneToOne.Entity.Employee;
+
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pg");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		Department d1 = new Department(1, "Tester", 3);
+		Department d2 = new Department(2, "Backend Developer", 5);
+		Department d3 = new Department(3, "Frontend Developer", 8);
+		
+		
+        Employee e1 = new Employee(1, "omkar", 18, d3);
+        Employee e2 = new Employee(2, "Chinamay", 22, d2);
+        Employee e3 = new Employee(3, "Rahul", 18, d1);
+		
+		
+		
+		et.begin();
+		em.persist(d1);
+		em.persist(d2);
+		em.persist(d3);
+		em.persist(e1);
+		em.persist(e2);
+		em.persist(e3);
+		et.commit();
+	}
+}
---
+++ Unidirectional_OneToOne/src/main/java/com/jsp/Unidirectional_OneToOne/Entity/Department.java
@@ -0,0 +1,53 @@
+package com.jsp.Unidirectional_OneToOne.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+
+	@Id
+	int id ;	
+	String department;
+	int count;
+	
+	
+	
+	public Department() {
+	}
+
+	public Department(int id, String department, int count) {
+		super();
+		this.id = id;
+		this.department = department;
+		this.count = count;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+	
+}
